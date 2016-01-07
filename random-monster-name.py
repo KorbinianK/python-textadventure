5fdb4c24d3b411f1c7b7b69e2b8452d5cd237234
korbinian <korbi87@gmail.com>
2016-01-07 21:02:06 +0100

    moved txt files to folders

--- random-monster-name.py
+++ random-monster-name.py
@@ -1,21 +1,21 @@
 import textwrap, random
 
-with open('name.txt', 'r') as f:
+with open('txt/namegenerator/name.txt', 'r') as f:
     names = f.read().splitlines()
     random.shuffle(names)
 
-with open('pre.txt', 'r') as f:
+with open('txt/namegenerator/pre.txt', 'r') as f:
     pre = f.read().splitlines()
 
-with open('adjective.txt', 'r') as f:
+with open('txt/namegenerator/adjective.txt', 'r') as f:
     adjectives = f.read().splitlines()
     random.shuffle(adjectives)
 
-with open('type.txt', 'r') as f:
+with open('txt/namegenerator/type.txt', 'r') as f:
     types = f.read().splitlines()
     random.shuffle(types)
 
-with open('objective.txt', 'r') as f:
+with open('txt/namegenerator/objective.txt', 'r') as f:
     objectives = f.read().splitlines()
     random.shuffle(objectives)
 
